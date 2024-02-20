e798a1570b38b26857a4d66c37afb07b0a4cafc4
Ikhtiyor <ihtiyor2655742@gmail.com>
2024-02-20 13:03:21 +0500

    alert added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
   const [clickedKeys, setClickedKeys] = useState([]);
@@ -7,10 +7,10 @@ function App() {
   const handleKeyPress = (event) => {
     const digit = event.key;
     // Check if the pressed key is a number
-    if (!isNaN(digit) && digit !== ' ') {
+    if (!isNaN(digit) && digit !== " ") {
       const newKey = { id: clickCount + 1, digit, keyCode: event.keyCode };
-      setClickedKeys(prevClickedKeys => [...prevClickedKeys, newKey]);
-      setClickCount(prevCount => prevCount + 1);
+      setClickedKeys((prevClickedKeys) => [...prevClickedKeys, newKey]);
+      setClickCount((prevCount) => prevCount + 1);
     } else {
       alert("You clicked a letter instead of a number!");
     }
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="App" tabIndex={0} onKeyDown={handleKeyPress}>
-      <h1>Press any key</h1>
+      <h1>Press me to start</h1>
       {clickedKeys.length > 0 && (
         <ul>
           {clickedKeys.map((key, index) => (
